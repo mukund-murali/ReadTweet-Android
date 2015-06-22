f27b9d3844e88c1000db1c4e3dbe668c59031877
Mukund Muralikrishnan <debay92@gmail.com>
2015-06-22 19:17:29 +0530

    added unadded file

---
+++ app/src/main/java/com/mukundvis/twitnews/models/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.mukundvis.twitnews.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by mukundvis on 22/06/15.
+ */
+public class ErrorResponse {
+
+    @SerializedName("message")
+    public String message;
+
+}
